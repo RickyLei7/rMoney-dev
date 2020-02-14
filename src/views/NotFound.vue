4ebfd189947c1d8c940339ec7dbad77578c02028
RickyLei <rickylei7@gmail.com>
2020-02-14 12:10:25 -0700

    add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>Page Not Found, Please check the address or connect Ricky.</div>
+    <div>
+      <router-link to="/">Return to Home Page</router-link>
+    </div>
+  </div>
+
+</template>
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
