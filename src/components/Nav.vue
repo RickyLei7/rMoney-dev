d9491c3a5d29766a0c2a7a2ee295b0cf58357bbe
RickyLei <rickylei7@gmail.com>
2020-02-17 09:55:51 -0700

    solve SVG fill issue.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,17 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       Money
     </router-link>
 
-    <router-link to="/label" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       Label
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
+
       <Icon name="statistics"/>
       Statistics
     </router-link>
@@ -44,6 +45,9 @@
     .icon{
       width: 32px;
       height: 32px;
+    }
+    > .item.selected{
+      color: red;
     }
   }
 
