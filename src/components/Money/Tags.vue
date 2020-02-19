1fb60fd87b444fdf5b514a30da4b2e85dfa6da6d
RickyLei <rickylei7@gmail.com>
2020-02-18 23:58:02 -0700

    Modularity：Money.vue to 4 components.

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>Add Tags</button>
+    </div>
+    <ul class="current">
+      <li>Cloth</li>
+      <li>Food</li>
+      <li>House</li>
+      <li>transport</li>
+      <li>Cloth</li>
+      <li>Food</li>
+      <li>House</li>
+      <li>transport</li>
+      <li>Cloth</li>
+      <li>Food</li>
+      <li>House</li>
+      <li>transport</li>
+    </ul>
+
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Tags.vue'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .tags {
+    font-size: 14px;
+    padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
+    > .current {
+      display: flex;
+      flex-wrap: wrap;
+      overflow: auto;
+      > li {
+        background: #d9d9d9;
+        $h: 24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 16px;
+        margin-right: 12px;
+        margin-top: 4px;
+      }
+    }
+    > .new {
+      padding-top: 16px;
+      button {
+        background: transparent;
+        border: none;
+        color: #999;
+        border-bottom: 1px solid;
+        padding: 0 3px;
+      }
+    }
+  }
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">Spend</li>
+      <li>Income</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Types'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+    > li {
+      width: 50%;
+      line-height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &.selected {
+        &::after {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          background: #333;
+        }
+      }
+    }
+  }
+</style>
