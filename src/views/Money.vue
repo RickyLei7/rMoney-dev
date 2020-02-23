bb3a706a8093cf105e91637284471673aa89072f
RickyLei <rickylei7@gmail.com>
2020-02-23 01:05:03 -0700

    change string of tag to {id,name}

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,10 +15,11 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
-  import model from '@/model';
+  import recordListModel from '@/models/recordListModel';
+  import tagListModel from '@/models/tagListModel';
 
-  const recordList = model.fetch();
-
+  const recordList = recordListModel.fetch();
+  const tagList = tagListModel.fetch();
 
   // const version = window.localStorage.getItem('version') || '0';
   //
@@ -38,7 +39,7 @@
     components: {Tags, Notes, Types, NumberPad},
   })
   export default class Money extends Vue {
-    tags = ['Cloth', 'Food', 'House', 'Transport', 'xxx'];
+    tags = tagList;
     recordList: RecordItem[] = recordList;
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
@@ -56,14 +57,14 @@
     }
 
     saveRecord() {
-      const record2: RecordItem = model.clone(this.record);
+      const record2: RecordItem = recordListModel.clone(this.record);
       record2.createAt = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      model.save(this.recordList);
+      recordListModel.save(this.recordList);
     }
   }
 </script>
