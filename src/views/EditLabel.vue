feb27c18ed9ad829db1422be43b0a6b6a7fdf8f6
RickyLei <rickylei7@gmail.com>
2020-02-24 01:19:45 -0700

    createId.ts

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -28,7 +28,7 @@
     components: {Button, FormItem}
   })
   export default class EditLabel extends Vue {
-    tag?: { id: string;name: string } = undefined;
+    tag?: { id: string; name: string } = undefined;
 
     created() {
       const id = this.$route.params.id;
@@ -50,7 +50,11 @@
 
     remove() {
       if (this.tag) {
-        tagListModel.remove(this.tag.id);
+        if (tagListModel.remove(this.tag.id)) {
+          this.$router.back();
+        } else {
+          window.alert('Delete fail');
+        }
       }
     }
 
