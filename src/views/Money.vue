3776750ab0cc2d3f3eed233635e52bc56c819491
RickyLei <rickylei7@gmail.com>
2020-03-01 21:22:34 -0700

    packaging record store.

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,11 +15,7 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
-  import recordListModel from '@/models/recordListModel';
-
-
-  const recordList = recordListModel.fetch();
+  import {Component} from 'vue-property-decorator';
 
 
   // const version = window.localStorage.getItem('version') || '0';
@@ -41,7 +37,7 @@
   })
   export default class Money extends Vue {
     tags = window.tagList;
-    recordList: RecordItem[] = recordList;
+    recordList = window.recordList;
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
     onUpdateTags(value: string[]) {
@@ -58,13 +54,9 @@
     }
 
     saveRecord() {
-      recordListModel.create(this.record);
+      window.createRecord(this.record);
     }
 
-    @Watch('recordList')
-    onRecordListChange() {
-      recordListModel.save();
-    }
   }
 </script>
 <style lang="scss">
