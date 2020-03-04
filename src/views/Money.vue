8e2b890abc6b404e5a2ccf6f8049009b3e75518b
RickyLei <rickylei7@gmail.com>
2020-03-04 16:11:29 -0700

    Delete Type.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,7 +14,7 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
+
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
@@ -37,7 +37,7 @@
   // // window.localStorage.setItem('version', '0.0.2');
 
   @Component({
-    components: {Tabs, Tags, FormItem, Types, NumberPad},
+    components: {Tabs, Tags, FormItem, NumberPad},
   })
   export default class Money extends Vue {
     get recordList() {
